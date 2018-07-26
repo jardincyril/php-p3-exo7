177c6372c72f53d0c13129e8548f1f59f3e23b83
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:49:37 +0200

    boucles

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo7</title>
+  </head>
+  <body>
+    <?php
+
+for ($i = 1; $i <= 100; $i+=15) {
+   echo 'on tient le bon bout. ';
+}
+?>
+  </body>
+</html>
